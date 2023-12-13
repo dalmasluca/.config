72942df69c6a620d3b4340408a355549611a7bef
dalmasluca <luca.dalmas@icloud.com>
2023-12-13 20:09:27 +0100

    AddScalaPlugin

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,5 +1,3 @@
--- Read the docs: https://www.lunarvim.org/docs/configuration
--- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 --
@@ -43,6 +41,10 @@ lvim.plugins = {
     config = true,
     -- Uncomment next line if you want to follow only stable versions
     version = "*"
+  },
+  {
+    "scalameta/nvim-metals",
+    dependencies = { "nvim-lua/plenary.nvim" }
   },
   {
     "L3MON4D3/LuaSnip",
